26256ac68bb662711c02b07b531c3633647e60d1
Chris Olivier <chris.olivier@derivco.co.uk>
2022-12-15 14:04:24 +0000

    Working simple publish and sub

--- MessageProcessing/Program.cs
+++ MessageProcessing/Program.cs
@@ -1,10 +1,33 @@
-﻿using NATS.Client;
+﻿using Microsoft.Extensions.Configuration;
+using NATS.Client;
+using System;
+using System.Threading;
+using System.Text;
 
 namespace MessageProcessing;
 class Program
 {
-    static void Main(string[] args)
+  protected readonly IConfiguration _configuration;
+  static void Main(string[] args)
+  {
+    // create a connection to the NATS server
+    IConnection connection = new ConnectionFactory().CreateConnection();
+
+    // subscribe to a subject
+    var sub = connection.SubscribeAsync("my_subject");
+    sub.MessageHandler += (sender, args) =>
     {
-        Console.WriteLine("Hello, World!");
-    }
+      Console.WriteLine($"Received message: {args.Message.Data}");
+    };
+
+    // publish a message to the subject
+    connection.Publish("my_subject", Encoding.UTF8.GetBytes("Hello NATS!"));
+
+    // wait for a key press to exit the app
+    Console.ReadKey();
+
+    // clean up
+    sub.Unsubscribe();
+    connection.Close();
+  }
 }
